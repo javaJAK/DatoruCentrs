1001bc06b3d8d40d50da5717ef07996a8f18ba6f
lietotajs <lietotajs@121-9>
2018-06-08 10:35:07 +0300

    pievienoju aktsdb

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ id SERIAL NOT NULL PRIMARY KEY,
 nosaukums VARCHAR (255));
 
 -- Tabula: AMATI
-create table if not exists amatI(
+create table if not exists amati(
 id serial not null primary key,
 nosaukums varchar(255));
 
@@ -23,11 +23,21 @@ amats_id integer,
 constraint amats_fk foreign key(amats_id) references amati(id));
 
 -- Tabula: REMONTA AKTI
-CREATE TABLE IF NOT EXISTS remonta_akti(id SERIAL NOT NULL PRIMARY KEY, 
-persona_id INTEGER,
+CREATE TABLE IF NOT EXISTS remonta_akti(
+id SERIAL NOT NULL PRIMARY KEY, 
+klients_id INTEGER,
+tehnikis_id INTEGER,
 darba_veids_id INTEGER,
+problema VARCHAR(255),
 datortehnika_id INTEGER,
-CONSTRAINT persona_fk FOREIGN KEY(persona_id) REFERENCES personas(id),
+serialais_numurs VARCHAR(255),
+piezimes VARCHAR(255),
+transports DOUBLE PRECISION,
+darba_ilgums DOUBLE PRECISION,
+darba_apraksts VARCHAR(255),
+datums DATE,
+CONSTRAINT klients_fk FOREIGN KEY(klients_id) REFERENCES personas(id),
+CONSTRAINT tehnikis_id FOREIGN KEY(klients_id) REFERENCES personas(id),
 CONSTRAINT darba_veids_fk FOREIGN KEY(darba_veids_id) REFERENCES darba_veidi(id),
 CONSTRAINT datortehnika_fk FOREIGN KEY(datortehnika_id) REFERENCES datortehnika(id));
 
